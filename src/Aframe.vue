f52a048b3589d97011e8df37c2b7514f73bf101c
SimonMeia <simon@meia.dev>
2023-03-08 16:05:39 +0100

    game description

--- src/Aframe.vue
+++ src/Aframe.vue
@@ -3,7 +3,6 @@
   import TheOnboarding from './components/TheOnboarding.vue';
   import TheScene from './components/TheScene.vue';
 
-  const scale = ref(1);
   const loaded = ref(false);
 </script>
 
@@ -11,8 +10,6 @@
   <TheOnboarding :loaded="loaded" />
 
   <!-- The DOM element of the overlay must be mounted before the A-Frame Scene is mounted -->
-  <!-- Otherwise the "webxr system" of the A-Frame scene wont find the DOM Element -->
-  <!-- <TheOverlay v-model="scale" id="overlay" /> -->
 
   <TheScene
     @loaded="loaded = true"
--- src/components/TheOnboarding.vue
+++ src/components/TheOnboarding.vue
@@ -1,124 +1,206 @@
 <script setup>
-  import { ref } from 'vue';
+import { ref } from "vue";
 
-  defineProps({
+defineProps({
     loaded: Boolean,
-  });
+});
 
-  const showOnboarding = ref(true);
+const showOnboarding = ref(true);
 
-  function enterScene() {
+function enterScene() {
     showOnboarding.value = false;
     if (AFRAME.utils.device.checkHeadsetConnected() && !AFRAME.utils.device.isMobile()) {
-      document.querySelector('a-scene').enterVR();
+        document.querySelector("a-scene").enterVR();
     }
-  }
+}
 </script>
 
 <template>
-  <div id="onboarding" v-if="showOnboarding">
-    <div>
-      <h1>VR Sandwiches</h1>
-      <p v-if="!loaded">loading...</p>
-      <button v-if="loaded" @click="enterScene()">Play</button>
-      <div class="licences">
-        <dl>
-          <dt>
-            <i>Included</i>
-          </dt>
-          <dt>
-            <a href="https://github.com/jure/aframe-blink-controls/" target="_blank">aframe-blink-controls</a>
-          </dt><dd>
-            <a href="https://github.com/jure/aframe-blink-controls/blob/main/LICENSE" target="_blank">MIT License</a>
-          </dd>
+    <div id="onboarding" v-if="showOnboarding">
+        <div>
+            <h1>VR Sandwiches</h1>
+            <p style="text-align: left;">
+                Bienvenue dans notre jeu de cuisine en réalité virtuelle ! Dans ce jeu, vous êtes le
+                chef et votre objectif est de préparer des sandwiches délicieux en suivant les
+                instructions de la recette affichée sur le bar. Pour assembler le sandwich, vous
+                devrez prendre les ingrédients nécessaires dans la cuisine et les placer
+                correctement sur l'assiette présente sur le bar.
+            </p>
+            <p style="text-align: left;">
+                Une fois que vous avez terminé, cliquez sur la sonnette pour soumettre votre
+                création pour vérification. Si vous avez commis une erreur, ne vous inquiétez pas,
+                vous pouvez jeter le sandwich dans la poubelle et recommencer. Faites preuve de
+                précision et de rapidité pour réussir chaque niveau et devenir le meilleur chef de
+                sandwich !
+            </p>
+            <p v-if="!loaded">loading...</p>
+            <button v-if="loaded" @click="enterScene()">Jouer</button>
+            <div class="licences">
+                <dl>
+                    <dt>
+                        <i>Included</i>
+                    </dt>
+                    <dt>
+                        <a href="https://github.com/jure/aframe-blink-controls/" target="_blank"
+                            >aframe-blink-controls</a
+                        >
+                    </dt>
+                    <dd>
+                        <a
+                            href="https://github.com/jure/aframe-blink-controls/blob/main/LICENSE"
+                            target="_blank"
+                            >MIT License</a
+                        >
+                    </dd>
 
-          <dt>
-            <a href=" https://github.com/AdaRoseCannon/aframe-xr-boilerplate" target="_blank">simple-navmesh-constraint</a>
-          </dt><dd>
-            By Ada Rose Cannon under MIT License
-          </dd>
-          <dt>
-            <a href="https://skfb.ly/ozHET" target="_blank">Furniture collection</a>
-          </dt><dd>
-            by <a href="https://sketchfab.com/charalamboscharalambous8" target="_blank">Harry Charalambous</a>
-            under <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank">CC BY 4.0</a>
-          </dd>
-          <dt>
-            <a href="https://skfb.ly/ozG7Q" target="_blank">Sandwich assembly</a>
-          </dt><dd>
-            by <a href="https://sketchfab.com/charalamboscharalambous8" target="_blank">Harry Charalambous</a>
-            under <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank">CC BY 4.0</a>
-          </dd>
-          <dt>
-            <a href="https://skfb.ly/opy8I" target="_blank">Food Equipment .Blend FREE Low Poly</a>
-          </dt><dd>
-            by <a href="https://sketchfab.com/LadyLionStudios" target="_blank">LordSamueliSolo</a>
-            under <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank">CC BY 4.0</a>
-          </dd>
-          <dt>
-            <a href="https://skfb.ly/onIzV" target="_blank">Desk Bell</a>
-          </dt><dd>
-            by <a href="https://sketchfab.com/clintonabbott.art" target="_blank">ClintonAbbott.Art</a>
-            under <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank">CC BY 4.0</a>
-          </dd>
-          <dt>
-            <a href="https://skfb.ly/6WRxV" target="_blank">Menu & Stand</a>
-          </dt><dd>
-            by <a href="https://sketchfab.com/libbycox" target="_blank">libbycox</a>
-            under <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank">CC BY 4.0</a>
-          </dd>
-          <dt>
-            <a href="https://skfb.ly/6XQI7" target="_blank">Waste Bin</a>
-          </dt><dd>
-            by <a href="https://sketchfab.com/GreenLineStudio" target="_blank">Lasse Harm</a>
-            under <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank">CC BY 4.0</a>
-          </dd>
-          <dt>
-            <a href="https://skfb.ly/6RKF8" target="_blank">Restaurant</a>
-          </dt><dd>
-            by <a href="https://sketchfab.com/uouoamy" target="_blank">Amy</a>
-            under <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank">CC BY 4.0</a>
-          </dd>
-          <dt>
-            <a href="https://skfb.ly/Nqwy" target="_blank">Cork Board</a>
-          </dt><dd>
-            by <a href="https://sketchfab.com/lianhaggis" target="_blank">lianhaggis</a>
-            under <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank">CC BY 4.0</a>
-          </dd>
-          <dt>
-            <a href="https://freesound.org/people/InspectorJ/sounds/415510/" target="_blank">Bell, Counter, A.wav</a>
-          </dt><dd>
-            by <a href="http://www.jshaw.co.uk/" target="_blank">Inspector</a>
-            under <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank">CC BY 4.0</a>
-          </dd>
-        </dl>
-      </div>
+                    <dt>
+                        <a
+                            href=" https://github.com/AdaRoseCannon/aframe-xr-boilerplate"
+                            target="_blank"
+                            >simple-navmesh-constraint</a
+                        >
+                    </dt>
+                    <dd>By Ada Rose Cannon under MIT License</dd>
+                    <dt>
+                        <a href="https://skfb.ly/ozHET" target="_blank">Furniture collection</a>
+                    </dt>
+                    <dd>
+                        by
+                        <a href="https://sketchfab.com/charalamboscharalambous8" target="_blank"
+                            >Harry Charalambous</a
+                        >
+                        under
+                        <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank"
+                            >CC BY 4.0</a
+                        >
+                    </dd>
+                    <dt>
+                        <a href="https://skfb.ly/ozG7Q" target="_blank">Sandwich assembly</a>
+                    </dt>
+                    <dd>
+                        by
+                        <a href="https://sketchfab.com/charalamboscharalambous8" target="_blank"
+                            >Harry Charalambous</a
+                        >
+                        under
+                        <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank"
+                            >CC BY 4.0</a
+                        >
+                    </dd>
+                    <dt>
+                        <a href="https://skfb.ly/opy8I" target="_blank"
+                            >Food Equipment .Blend FREE Low Poly</a
+                        >
+                    </dt>
+                    <dd>
+                        by
+                        <a href="https://sketchfab.com/LadyLionStudios" target="_blank"
+                            >LordSamueliSolo</a
+                        >
+                        under
+                        <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank"
+                            >CC BY 4.0</a
+                        >
+                    </dd>
+                    <dt>
+                        <a href="https://skfb.ly/onIzV" target="_blank">Desk Bell</a>
+                    </dt>
+                    <dd>
+                        by
+                        <a href="https://sketchfab.com/clintonabbott.art" target="_blank"
+                            >ClintonAbbott.Art</a
+                        >
+                        under
+                        <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank"
+                            >CC BY 4.0</a
+                        >
+                    </dd>
+                    <dt>
+                        <a href="https://skfb.ly/6WRxV" target="_blank">Menu & Stand</a>
+                    </dt>
+                    <dd>
+                        by
+                        <a href="https://sketchfab.com/libbycox" target="_blank">libbycox</a> under
+                        <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank"
+                            >CC BY 4.0</a
+                        >
+                    </dd>
+                    <dt>
+                        <a href="https://skfb.ly/6XQI7" target="_blank">Waste Bin</a>
+                    </dt>
+                    <dd>
+                        by
+                        <a href="https://sketchfab.com/GreenLineStudio" target="_blank"
+                            >Lasse Harm</a
+                        >
+                        under
+                        <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank"
+                            >CC BY 4.0</a
+                        >
+                    </dd>
+                    <dt>
+                        <a href="https://skfb.ly/6RKF8" target="_blank">Restaurant</a>
+                    </dt>
+                    <dd>
+                        by <a href="https://sketchfab.com/uouoamy" target="_blank">Amy</a> under
+                        <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank"
+                            >CC BY 4.0</a
+                        >
+                    </dd>
+                    <dt>
+                        <a href="https://skfb.ly/Nqwy" target="_blank">Cork Board</a>
+                    </dt>
+                    <dd>
+                        by
+                        <a href="https://sketchfab.com/lianhaggis" target="_blank">lianhaggis</a>
+                        under
+                        <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank"
+                            >CC BY 4.0</a
+                        >
+                    </dd>
+                    <dt>
+                        <a
+                            href="https://freesound.org/people/InspectorJ/sounds/415510/"
+                            target="_blank"
+                            >Bell, Counter, A.wav</a
+                        >
+                    </dt>
+                    <dd>
+                        by <a href="http://www.jshaw.co.uk/" target="_blank">Inspector</a> under
+                        <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank"
+                            >CC BY 4.0</a
+                        >
+                    </dd>
+                </dl>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <style scoped>
-  h1 { font-size: 1.5rem }
-  a {
+h1 {
+    font-size: 1.5rem;
+}
+a {
     color: #eee;
     text-decoration: none;
-  }
-  .licences {
+}
+.licences {
     margin: 2rem 0;
     font-size: 1rem;
     text-align: left;
-  }
-  .licences dt {
+}
+.licences dt {
     padding-top: 0.75rem;
     font-size: 0.9rem;
     font-weight: bold;
-  }
-  .licences dd {
+}
+.licences dd {
     margin-left: 0;
     font-size: 0.8rem;
-  }
+}
 
-  #onboarding {
+#onboarding {
     position: absolute;
     top: 0;
     left: 0;
@@ -130,8 +212,8 @@
     font-family: monospace;
     z-index: 10000;
     overflow: auto;
-  }
-  #onboarding > * {
+}
+#onboarding > * {
     margin: 0 auto;
     max-width: 50rem;
     width: calc(100vw - 10rem);
@@ -139,8 +221,8 @@
     border-radius: 0.3rem;
     padding: 1rem;
     font-size: 1.3rem;
-  }
-  #onboarding button {
+}
+#onboarding button {
     font-size: 1.3rem;
     padding: 0.5rem 1rem;
     border-radius: 0.3rem;
@@ -148,5 +230,5 @@
     color: black;
     border: none;
     cursor: pointer;
-  }
+}
 </style>
